b66a6f34980519175c2300bf9bfb7ede278dec54
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-03-31 16:14:20 +0300

    task 1.3 JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
 
+/**
+ *Class Calculate, solution of the task 1.3. JavaDoc. Code convention
+ *@author dbobrov
+ *@since 31.03.2016 
+*/
 public class Calculate {
+
+/**
+ * Output in the console string "Hello world."
+*/	
 	public static void main (String[] args) {
 		System.out.println("Hello world.");
 	}
