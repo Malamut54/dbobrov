b97cbcacc8b80f4fdba4c5419868c25b9e3c72bb
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-04-01 15:51:46 +0300

    Task 1.4.1 Junit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,17 +1,17 @@
 package ru.job4j;
 
 /**
- *Class Calculate, solution of the task 1.3. JavaDoc. Code convention
- *@author dbobrov
- *@since 31.03.2016 
+*Class Calculate, solution of the task 1.3. JavaDoc. Code convention
+*@author dbobrov
+*@since 31.03.2016
 */
 public class Calculate {
 
 /**
- * Output in the console string "Hello world."
- *@param args
-*/	
-	public static void main (String[] args) {
+* Output in the console string "Hello world.".
+*@param args **command-line arguments**
+*/
+	public static void main(String[] args) {
 		System.out.println("Hello world.");
 	}
 }
