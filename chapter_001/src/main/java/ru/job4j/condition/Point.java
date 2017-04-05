134a31921d76932800c8d871a1bbf2e16aeb0824
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-04-05 07:53:05 +0300

    Task 3.2 Condition point

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,48 @@
+package ru.job4j.condition;
+
+/**
+*Point class of the task 3.2
+*@author Dmitriy Bobrov (mailto:bobrov.dmitriy@gmail.com)
+*@since 0.1
+*/
+
+public class Point {
+/**
+*Class Point Describes the point.
+*/
+   private int x;
+   private int y;
+/**
+*Point constructor.
+*@param x X point coordinate
+*@param y Y point coordinate
+*/ 
+
+   public  Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+  }
+
+/**
+*getter for coordinate x.
+*/  
+  public int getX() {
+      return this.x;
+  }
+ /**
+*getter for coordinate y.
+*/ 
+
+  public int getY() {
+     return this.y;
+  }
+/**
+*Check is Point  exists on function (y(x) = a * x + b).
+*@param aValue a value
+*@param bValue b value
+*@return true if point exists on function
+*/
+  public boolean is(int a, int b) {
+    return this.y == a * this.x + b;
+  }
+}
