2ba7cdd55febbcd915ef91f3d5f5f4ca1611a5a3
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-04-06 10:21:47 +0300

    Task 3.3 Triangle

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+/**
+*Triangle class of the task 3.3.
+*@author Dmitriy Bobrov (mailto:bobrov.dmitriy@gmail.com)
+*@since 0.1
+*/
+
+public class Triangle {
+	private Point a;
+	private Point b;
+	private Point c;
+
+	public Triangle(Point a, Point b, Point c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public double area() {
+		Point a = new Point;
+		Point b = new Point;
+		Point c = new Point;
+	}
+}
