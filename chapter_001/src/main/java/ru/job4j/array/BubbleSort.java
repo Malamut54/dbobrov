27f44caea1897dbb6a49702964774b7470953823
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-04-10 14:29:02 +0300

    Task 5.1 Bubble sort

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -19,9 +19,9 @@ public class BubbleSort {
 		for (int i = array.length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (array[j] > array[j + 1]) {
-                    int t = array[j];
+                    int tmp = array[j];
                     array[j] = array[j + 1];
-                    array[j + 1] = t;
+                    array[j + 1] = tmp;
                 }
             }
         }
