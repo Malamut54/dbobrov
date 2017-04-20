4aa8a45c330f0d92bcc1b466cb38f0a275202af2
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-04-20 09:52:35 +0300

    Professions

--- chapter_002/src/main/java/ru/job4j/proffesions/Proffesions.java
+++ chapter_002/src/main/java/ru/job4j/proffesions/Proffesions.java
@@ -4,5 +4,14 @@ package ru.job4j.proffesions;
  * Create proffesions.
  */
 public class Proffesions {
+/**
+ *Class Profeesions. Describes the profession.
+ */
+    private String name;
+    private int age;
+    private boolean gender;
 
+    public void Work(){
+
+    }
 }
