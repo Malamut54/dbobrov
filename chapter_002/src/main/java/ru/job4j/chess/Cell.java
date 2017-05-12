0ab23198a02682ef846d7675c8d13b208c3d1617
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-05-12 10:31:33 +0300

    Tracker_5

---
+++ chapter_002/src/main/java/ru/job4j/chess/Cell.java
@@ -0,0 +1,8 @@
+package ru.job4j.chess;
+
+/**
+ * Created by Malamut on 5/11/2017.
+ */
+public class Cell {
+
+}
--- chapter_002/src/main/java/ru/job4j/chess/Figure.java
+++ chapter_002/src/main/java/ru/job4j/chess/Figure.java
@@ -4,5 +4,14 @@ package ru.job4j.chess;
  * Final task. Chess.
  */
 public abstract class Figure {
+    String name;
+    final Cell StartPosition;
 
+    public Figure(Cell startPosition) {
+        this.StartPosition = startPosition;
+    }
+
+    Cell[] way(Cell dist) {
+
+    }
 }
