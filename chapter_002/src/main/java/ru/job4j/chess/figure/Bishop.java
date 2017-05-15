6f366159db26bd7595c4703a77aa5703eb053a35
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-05-15 10:51:16 +0300

    Tracker_5

--- chapter_002/src/main/java/ru/job4j/chess/figure/Bishop.java
+++ chapter_002/src/main/java/ru/job4j/chess/figure/Bishop.java
@@ -7,22 +7,27 @@ import ru.job4j.chess.exception.ImpossibleMoveException;
  * Created by kvazimoda on 12.05.2017.
  */
 public class Bishop extends Figure {
-    private Cell position;
-    private Cell[] waysToDist = new Cell[18];
+    private Cell position = new Cell(2, 0);
+    private Cell[] waysToDist = new Cell[9];
     private int count = 0;
 
     public Bishop(Cell position, String name) {
         super(position, name);
     }
 
-    Cell[] way(Cell dist) throws ImpossibleMoveException {
+    @Override
+    public Cell[] way(Cell dist) throws ImpossibleMoveException {
         if (0 <= dist.getX() && LENGTHBOARD <= dist.getX() &&
                 0 <= dist.getY() && LENGTHBOARD <= dist.getY()) {
 
         }
-        if (dist.getY() - position.getY() == dist.getX() - position.getX()) {
-
+        if (position.getX() - dist.getX() == position.getY() + dist.getY()) {
         }
+        if (dist.getX() - position.getX() == dist.getY() - position.getY()) {
+        }
+        if (position.getX() - dist.getX() == position.getY() - dist.getY()) {
+        }
+
         return new Cell[0];
     }
 }
