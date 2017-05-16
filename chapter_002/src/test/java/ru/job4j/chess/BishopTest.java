6cf9ad1b14c2610bf0696c88d6c58fcd3e6f51e5
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-05-16 20:36:30 +0300

    Chess_begin

---
+++ chapter_002/src/test/java/ru/job4j/chess/BishopTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.chess;
+
+import org.junit.Test;
+import ru.job4j.chess.exception.FigureNotFoundException;
+import ru.job4j.chess.exception.ImpossibleMoveException;
+import ru.job4j.chess.exception.OccupiedWayException;
+import ru.job4j.chess.figure.Bishop;
+import ru.job4j.chess.figure.Figure;
+
+public class BishopTest {
+    @Test(expected = ImpossibleMoveException.class)
+    public void WhenWentBeyondTheBoard() throws FigureNotFoundException, OccupiedWayException, ImpossibleMoveException {
+        Figure figure = new Bishop(new Cell(2, 1));
+        figure.way(new Cell(10, 10));
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void WhenFigureNotFound() throws OccupiedWayException, ImpossibleMoveException, FigureNotFoundException {
+        Board board = new Board();
+        Cell source = new Cell(2, 1);
+        Cell destination = new Cell(7, 1);
+        board.move(source, destination);
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void WhenFigureMakeImpossibleMove() throws OccupiedWayException, ImpossibleMoveException, FigureNotFoundException {
+        Board board = new Board();
+        Cell source = new Cell(2, 1);
+        Cell destination = new Cell(2, 3);
+        board.move(source, destination);
+
+    }
+
+
+}
