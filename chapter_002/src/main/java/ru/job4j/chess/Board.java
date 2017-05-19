563f103a2c8ab76fef80fee64ac52bb68c63c894
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-05-19 18:31:23 +0300

    Chess_bishop

--- chapter_002/src/main/java/ru/job4j/chess/Board.java
+++ chapter_002/src/main/java/ru/job4j/chess/Board.java
@@ -5,27 +5,40 @@ import ru.job4j.chess.exception.*;
 
 public class Board {
     Figure figure;
-    Figure bishop1 = new Bishop(new Cell(2, 1));
-    Figure bishop2 = new Bishop(new Cell(4, 3));
-    Figure bishop3 = new Bishop(new Cell(3, 8));
-    Figure bishop4 = new Bishop(new Cell(6, 1));
-    Figure[] figures = {bishop1, bishop2, bishop3, bishop4};
+    //    Figure bishop1 = new Bishop(new Cell(2, 1));
+//    Figure bishop2 = new Bishop(new Cell(4, 3));
+//    Figure bishop3 = new Bishop(new Cell(3, 8));
+//    Figure bishop4 = new Bishop(new Cell(6, 1));
+//    Figure[] figures = {bishop1, bishop2, bishop3, bishop4};
+    private Figure[] figures = new Figure[32];
+
 
     public Figure[] getFigures() {
         return figures;
     }
 
+    public void fillBoard() {
+        figures[0] = new Bishop(new Cell(2, 1));
+        figures[1] = new Bishop(new Cell(4, 3));
+        figures[2] = new Bishop(new Cell(3, 8));
+        figures[3] = new Bishop(new Cell(6, 1));
+    }
+
     boolean move(Cell source, Cell dist) throws ImpossibleMoveException, OccupiedWayException, FigureNotFoundException {
         int positions = 0;
         for (int i = 0; i < this.figures.length; ) {
-            if (figures[i].getPosition().getX() == source.getX() && figures[i].getPosition().getY() == source.getY()) {
-                positions = i;
-                this.figure = figures[i];
+            if (figures == null) {
                 break;
-            } else if (i == figures.length - 1) {
-                throw new FigureNotFoundException("Figure not found");
-            } else if (figures[i].getPosition().getX() != source.getX() || figures[i].getPosition().getY() != source.getY()) {
-                i++;
+            } else {
+                if (figures[i].getPosition().getX() == source.getX() && figures[i].getPosition().getY() == source.getY()) {
+                    positions = i;
+                    this.figure = figures[i];
+                    break;
+                } else if (i == figures.length - 1) {
+                    throw new FigureNotFoundException("Figure not found");
+                } else if (figures[i].getPosition().getX() != source.getX() || figures[i].getPosition().getY() != source.getY()) {
+                    i++;
+                }
             }
         }
 
--- chapter_002/src/test/java/ru/job4j/chess/BishopTest.java
+++ chapter_002/src/test/java/ru/job4j/chess/BishopTest.java
@@ -11,15 +11,19 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class BishopTest {
+
+
     @Test(expected = ImpossibleMoveException.class)
     public void WhenWentBeyondTheBoard() throws FigureNotFoundException, OccupiedWayException, ImpossibleMoveException {
         Figure figure = new Bishop(new Cell(2, 1));
         figure.way(new Cell(10, 10));
+
     }
 
     @Test(expected = FigureNotFoundException.class)
     public void WhenFigureNotFound() throws OccupiedWayException, ImpossibleMoveException, FigureNotFoundException {
         Board board = new Board();
+        board.fillBoard();
         Cell source = new Cell(3, 1);
         Cell destination = new Cell(7, 1);
         board.move(source, destination);
@@ -28,6 +32,7 @@ public class BishopTest {
     @Test(expected = ImpossibleMoveException.class)
     public void WhenFigureMakeImpossibleMove() throws OccupiedWayException, ImpossibleMoveException, FigureNotFoundException {
         Board board = new Board();
+        board.fillBoard();
         Cell source = new Cell(3, 8);
         Cell destination = new Cell(5, 7);
         board.move(source, destination);
@@ -37,6 +42,7 @@ public class BishopTest {
     @Test(expected = OccupiedWayException.class)
     public void WhenFigureGoThroughtOccupiedCell() throws OccupiedWayException, ImpossibleMoveException, FigureNotFoundException {
         Board board = new Board();
+        board.fillBoard();
         Cell source = new Cell(2, 1);
         Cell destination = new Cell(6, 5);
         board.move(source, destination);
@@ -45,6 +51,7 @@ public class BishopTest {
     @Test
     public void WhenFigureSuccesfullyMakeMove() throws OccupiedWayException, ImpossibleMoveException, FigureNotFoundException {
         Board board = new Board();
+        board.fillBoard();
         Cell source = new Cell(6, 1);
         Cell destination = new Cell(7, 2);
         board.move(source, destination);
