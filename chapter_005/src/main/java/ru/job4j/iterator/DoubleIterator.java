d12ec41ac47e3770cf03dd0587bf30c647d4b7dd
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-06-28 19:11:59 +0300

    Double Iter

---
+++ chapter_005/src/main/java/ru/job4j/iterator/DoubleIterator.java
@@ -0,0 +1,22 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+
+/**
+ * TODO: comment
+ *
+ * @author dbobrov
+ * @since 28.06.2017
+ */
+
+public class DoubleIterator implements Iterator {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+}
