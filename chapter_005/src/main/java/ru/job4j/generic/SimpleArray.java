79758c2ad3c66ed9abb4988ba1803f19ef22a513
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-07-07 12:42:06 +0300

    5.2.1. Реализовать SimpleArray<T> [#156]

--- chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
+++ chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -1,33 +1,79 @@
 package ru.job4j.generic;
 
 /**
- * TODO: comment
+ * Chapter Generic.
  *
  * @author Dmitriy Bobrov (bobrov.dmitriy@gmail.com)
  * @since 06.07.2017
  */
 
+/**
+ * Class Simple Array.
+ *
+ * @param <E> Input Generic.
+ */
 public class SimpleArray<E> {
-    E[] array;
-    int index = 0;
+    /**
+     * Private field.
+     */
+    private Object[] array;
+    /**
+     * Private filed.
+     */
+    private int index = 0;
 
+    /**
+     * Constructor.
+     * @param sizeArray - set size of array.
+     */
     public SimpleArray(int sizeArray) {
         this.array = new Object[sizeArray];
     }
 
+    /**
+     * Add some value for array.
+     * @param value - input value.
+     */
     public void add(E value) {
-
+        this.array[index++] = value;
     }
 
-    public void update(E value) {
-
+    /**
+     * Update value from array.
+     *
+     * @param value    - value for change.
+     * @param newValue - new value.
+     */
+    public void update(E value, E newValue) {
+        for (int i = 0; i < this.array.length; i++) {
+            if (this.array[i].equals(value)) {
+                this.array[i] = newValue;
+            }
+        }
     }
 
+    /**
+     * Delete value from array.
+     * @param value - value for delete.
+     */
     public void delete(E value) {
-
+        for (int i = 0; i < this.array.length; i++) {
+            if (this.array[i].equals(value)) {
+                System.arraycopy(this.array, i + 1, this.array, i, this.array.length - 1);
+                this.array[array.length - 1] = null;
+                this.index--;
+                break;
+            }
+        }
     }
 
-    public <E> E get(int index) {
-        return E;
+    /**
+     * Get value at index.
+     *
+     * @param index - input index.
+     * @return value.
+     */
+    public E get(int index) {
+        return (E) this.array[index];
     }
 }
--- chapter_005/src/test/java/ru/job4j/generic/SImpeArrayTest.java
+++ chapter_005/src/test/java/ru/job4j/generic/SImpeArrayTest.java
@@ -2,19 +2,67 @@ package ru.job4j.generic;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
- * TODO: comment
+ * Chapter Generic.
  *
  * @author Dmitriy Bobrov (bobrov.dmitriy@gmail.com)
  * @since 06.07.2017
  */
 
 public class SImpeArrayTest {
+    /**
+     * Test Add.
+     */
     @Test
     public void whenAddStringToSimpleArray() {
         SimpleArray<String> arr = new SimpleArray<String>(1);
+        arr.add("Test");
         String expected = "Test";
         String result = arr.get(0);
-
+        assertThat(result, is(expected));
     }
+
+    /**
+     * Test Update.
+     */
+    @Test
+    public void whenUpdateValueStringFromExistEntry() {
+        SimpleArray<String> arr = new SimpleArray<String>(1);
+        arr.add("Test");
+        arr.update("Test", "Java");
+        String result = arr.get(0);
+        String expected = "Java";
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test Delete.
+     */
+    @Test
+    public void whenDeleteValueStringAndReturnNull() {
+        SimpleArray<Integer> arr = new SimpleArray<Integer>(3);
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.delete(1);
+        Integer expected = 2;
+        Integer result = arr.get(0);
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test Get.
+     */
+    @Test
+    public void whenAddValueTheGetHim() {
+        SimpleArray<Integer> arr = new SimpleArray<Integer>(1);
+        arr.add(1);
+        Integer result = arr.get(0);
+        Integer expected = 1;
+        assertThat(result, is(expected));
+    }
+
 }
