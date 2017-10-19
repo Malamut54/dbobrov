d53deac1ccdceef03b2bc876aad5b24911f2f8ab
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-10-19 11:42:56 +0300

    класс Count и метод int increment() [#1102]

---
+++ chapter_007/src/main/java/ru/job4j/monitor/Count.java
@@ -0,0 +1,52 @@
+package ru.job4j.monitor;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * Multi Count.
+ *
+ * @author Dmitriy Bobrov (bobrov.dmitriy@gmail.com)
+ * @since 18.10.2017
+ *
+ */
+
+@ThreadSafe
+class Count implements Runnable {
+
+    /**
+     * value for count.
+     */
+    @GuardedBy("this")
+    private int val;
+
+    /**
+     * Increment and print value.
+     */
+    public void increment() {
+        val++;
+        System.out.println(val);
+    }
+    @Override
+    public void run() {
+        synchronized (this) {
+            increment();
+        }
+    }
+}
+
+/**
+ * Demo class.
+ */
+class Demo {
+    /**
+     * Main method.
+     * @param args input args.
+     */
+    public static void main(String[] args) {
+        Count count = new Count();
+        for (int i = 0; i < 10; i++) {
+            new Thread(count).start();
+        }
+    }
+}
---
+++ chapter_007/src/main/java/ru/job4j/monitor/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Multi Count.
+ *
+ * @author Dmitriy Bobrov (bobrov.dmitriy@gmail.com)
+ * @since 19.10.2017
+ */
+
+package ru.job4j.monitor;
