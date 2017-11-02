cb69d8435b07ed7efaaf2574c7c9d854a0c2f405
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2017-11-02 18:47:43 +0300

    3. Реализовать собственный механизм блокировок Lock.  [#1100]

--- chapter_007/src/main/java/ru/job4j/lock/Demo.java
+++ chapter_007/src/main/java/ru/job4j/lock/Demo.java
@@ -1,23 +1,32 @@
 package ru.job4j.lock;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * TODO: comment
+ * Task Lock.
  *
  * @author Dmitriy Bobrov (bobrov.dmitriy@gmail.com)
  * @since 02.11.2017
  */
 
 public class Demo implements Runnable {
-    Incr incr = new Incr();
+    /**
+     * Class field.
+     */
+    private Increment incr = new Increment();
 
     @Override
     public void run() {
-        incr.increment();
+        try {
+            incr.increment();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * Main method.
+     *
+     * @param args input args.
+     */
     public static void main(String[] args) {
         Demo demo = new Demo();
         for (int i = 0; i < 10; i++) {
@@ -26,11 +35,29 @@ public class Demo implements Runnable {
     }
 }
 
-class Incr {
+/**
+ * Demonstrate SimpleLock.
+ */
+class Increment {
+    /**
+     * value for increment.
+     */
     private int i;
+    /**
+     * Realization SimpleLock.
+     */
+    private SimpleLock lock = new SimpleLock();
 
-    public void increment() {
+    /**
+     * Method increment the value.
+     *
+     * @throws InterruptedException exception.
+     */
+    public void increment() throws InterruptedException {
+        lock.lock();
         i++;
+        Thread.sleep((int) (Math.random() * 100));
         System.out.println(i);
+        lock.unlock();
     }
 }
--- chapter_007/src/main/java/ru/job4j/lock/SimpleLock.java
+++ chapter_007/src/main/java/ru/job4j/lock/SimpleLock.java
@@ -1,15 +1,23 @@
 package ru.job4j.lock;
 
 /**
- * TODO: comment
+ * Task Lock.
  *
  * @author Dmitriy Bobrov (bobrov.dmitriy@gmail.com)
  * @since 02.11.2017
  */
 
 public class SimpleLock {
+    /**
+     * Lock or unlock.
+     */
     private boolean isLocked = false;
 
+    /**
+     * Method lock job.
+     *
+     * @throws InterruptedException exception.
+     */
     public synchronized void lock() throws InterruptedException {
         while (isLocked) {
             wait();
@@ -17,6 +25,9 @@ public class SimpleLock {
         isLocked = true;
     }
 
+    /**
+     * Method unlock job.
+     */
     public synchronized void unlock() {
         isLocked = false;
         notifyAll();
