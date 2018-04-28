e2f98e6809c244dbcb5b7e07a3cbf2e8731aede4
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2018-04-28 16:50:18 +0300

    hibernate_begin

---
+++ chapter_010/src/main/java/ru/job4j/todo/model/entity/Item.java
@@ -0,0 +1,44 @@
+package ru.job4j.todo.model.entity;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+import java.sql.Timestamp;
+
+public class Item {
+    private int id;
+    private String desc;
+    private Timestamp created;
+    private boolean done;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
